f1ca8177c3d1c0450a2fa8bc5999d82b30f111da
anatolie_ghebea <anatolie.ghebea@pictastudio.com>
2023-04-09 19:46:24 +0200

    edit instal script

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,7 +14,11 @@ echo "Unpacking..."
 tar -xzf elasticsearch-8.6.2-linux-x86_64.tar.gz
 rm -rf elasticsearch-8.6.2-linux-x86_64.tar.gz*
 
-export ES_HOME="$PWD/elasticsearch-8.6.2"
+echo 'Setting home path'
+
+echo 'export ES_HOME="~/elasticsearch-8.6.2"' >> ~/.bashrc
+source ~/.bashrc
+
 echo $ES_HOME
 
 cd ./els-single-node/config
