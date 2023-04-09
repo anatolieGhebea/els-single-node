829a7908196273c7962f883599c69e9ad547b457
anatolie_ghebea <anatolie.ghebea@pictastudio.com>
2023-04-09 19:26:43 +0200

    change path

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,10 +17,12 @@ rm -rf elasticsearch-8.6.2-linux-x86_64.tar.gz*
 export ES_HOME="$PWD/elasticsearch-8.6.2"
 echo $ES_HOME
 
-cd ../config
+cd ./els-single-node/config
 cp elasticsearch.yml.example elasticsearch.yml
 cp jvm_override.options.example jvm_override.options
 cd ../scripts
+
+pwd
 
 echo "Done!"
 echo "WARNING!! \nBefore running 'setconfig.sh' be sure to edit the settings in \nelasticsearch.yml \njvm_override.options \n"
